86e5e2194912faa1c5450c13ed8effbbd2aa1394
andreventuravale <andreventuravale@gmail.com>
2023-05-18 01:43:44 -0300

    index.mjs |

--- index.mjs
+++ index.mjs
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
 
-import {spawnSync} from 'node:child_process';
-import {existsSync, readFileSync, writeFileSync} from 'node:fs';
+import {spawn} from 'node:child_process';
+import {promises as fsPromises} from 'node:fs';
 import {join, resolve} from 'node:path';
 import process from 'node:process';
 import minimist from 'minimist';
 import {get} from 'lodash-es';
 import {parse, stringify} from 'yaml';
 import yn from 'yn';
+
+const {readFile, stat, writeFile} = fsPromises;
+
+const fileExists = async path => {
+	try {
+		await stat(path);
+
+		return true;
+	} catch {
+		return false;
+	}
+};
 
 let {root: optRoot, config: optConfig, 'fill-gaps': optFillGaps} = minimist(process.argv);
 
@@ -22,23 +34,24 @@ if (!optConfig) {
 }
 
 const wpath = resolve(optRoot);
-const configYaml = readFileSync(optConfig, 'utf8');
+const configYaml = await readFile(optConfig, 'utf8');
 const config = parse(configYaml);
 
-const jsonFile = file.bind(
-	{},
+const jsonFile = await makeFile(
 	'utf8',
-	raw => JSON.parse(raw),
-	parsed => JSON.stringify(parsed, null, 2),
+	async raw => JSON.parse(raw),
+	async parsed => JSON.stringify(parsed, null, 2),
 );
 
 const rootPkg = jsonFile(
 	join(wpath, 'package.json'),
 );
 
-rootPkg.content.name = config.name;
+await rootPkg.modify(async content => {
+	content.name = config.name;
+});
 
-rootPkg.save();
+await rootPkg.save();
 
 for await (const [wname, wnode] of Object.entries(config.workspaces)) {
 	process.chdir(wpath);
@@ -49,9 +62,11 @@ for await (const [wname, wnode] of Object.entries(config.workspaces)) {
 }
 
 async function visitNodes({config, wname, wnode, packageNodes, wpath, path}) {
-	for await (const [palias, packageNode] of Object.entries(packageNodes ?? {})) {
-		await visitNode({config, wname, wnode, palias, packageNode, wpath, path});
-	}
+	await Promise.all(
+		Object.entries(packageNodes ?? {}).map(async ([palias, packageNode]) => {
+			await visitNode({config, wname, wnode, palias, packageNode, wpath, path});
+		}),
+	);
 }
 
 async function visitNode({config, wname, wnode, palias, packageNode, wpath, path}) {
@@ -75,12 +90,12 @@ async function visitNode({config, wname, wnode, palias, packageNode, wpath, path
 
 			const pkgPath = join(pkgDir, 'package.json');
 
-			if (!existsSync('package.json')) {
+			if (!await fileExists('package.json')) {
 				const tmpl = JSON.parse(config.template);
 
 				tmpl.name = `@${wname}/${packageName}`;
 
-				writeFileSync(
+				await	writeFile(
 					pkgPath,
 					JSON.stringify(tmpl, null, 2),
 					'utf8',
@@ -89,7 +104,9 @@ async function visitNode({config, wname, wnode, palias, packageNode, wpath, path
 
 			const pkg = await jsonFile(pkgPath);
 
-			pkg.name = `@${wname}/${packageName}`;
+			await pkg.modify(async content => {
+				content.name = `@${wname}/${packageName}`;
+			});
 
 			console.group(`[${palias}]`, ':', pkg.name);
 
@@ -99,23 +116,31 @@ async function visitNode({config, wname, wnode, palias, packageNode, wpath, path
 				.map(([, attributes]) => attributes);
 
 			for await (const attributes of attributesList) {
-				for await (const [ref, {'save-peer': savePeer}] of Object.entries(attributes.references ?? {})) {
+				for await (const [ref, {'is-dev': isDev = 'no', 'save-peer': savePeer = 'no'}] of Object.entries(attributes.references ?? {})) {
 					await shell`${
-						[config.manager, 'install', `${ref}@${config.references?.[ref]?.version ?? 'latest'}`, '--save', yn(savePeer) ? '--save-peer' : '']
+						[
+							config.manager,
+							'install',
+							`${ref}@${config.references?.[ref]?.version ?? 'latest'}`,
+							yn(isDev) ? '--save-dev' : '--save',
+							yn(savePeer) ? '--save-peer' : '',
+						]
 							.filter(Boolean)
 							.join(' ')
 					}`;
 
-					pkg.invalidate();
+					await pkg.invalidate();
 				}
 			}
 
 			for await (const [rpath] of Object.entries(refs)) {
 				const rpackageName = wnode?.names[rpath];
 
-				pkg.dependencies = pkg.dependencies ?? {};
+				await pkg.modify(async content => {
+					content.dependencies = content.dependencies ?? {};
 
-				pkg.dependencies[`@${wname}/${rpackageName}`] = `${config.protocol}${`@${wname}/${rpackageName}`}`;
+					content.dependencies[`@${wname}/${rpackageName}`] = `${config.protocol}${`@${wname}/${rpackageName}`}`;
+				});
 
 				console.log('->', rpath);
 			}
@@ -123,7 +148,7 @@ async function visitNode({config, wname, wnode, palias, packageNode, wpath, path
 			if (Object.entries(children).length === 0) {
 				wnode.names[apath] = '';
 
-				writeFileSync(join(originalDir, optConfig), stringify(config, {nullStr: ''}), 'utf8');
+				await	writeFile(join(originalDir, optConfig), stringify(config, {nullStr: ''}), 'utf8');
 			}
 		} else {
 			console.group(`[${palias}]`);
@@ -135,43 +160,61 @@ async function visitNode({config, wname, wnode, palias, packageNode, wpath, path
 	}
 }
 
-async function shell(tpl = [], ...args) {
-	const a = tpl.slice(0);
-	const b = args.slice(0);
-	const script = [];
+function shell(tpl = [], ...args) {
+	return new Promise((resolve, reject) => {
+		const a = tpl.slice(0);
+		const b = args.slice(0);
+		const script = [];
 
-	while (a.length > 0 || b.length > 0) {
-		script.push(
-			a.shift() ?? '',
-			b.shift() ?? '',
-		);
-	}
+		while (a.length > 0 || b.length > 0) {
+			script.push(
+				a.shift() ?? '',
+				b.shift() ?? '',
+			);
+		}
 
-	const cmd = script.join('');
+		const cmd = script.join('');
 
-	console.log('$', cmd);
+		console.log('$', cmd);
 
-	spawnSync(cmd, {cwd: process.cwd(), stdio: 'inherit', shell: true});
+		const proc = spawn(cmd, {cwd: process.cwd(), stdio: 'inherit', shell: true});
+
+		let error;
+
+		proc.on('error', error_ => {
+			error = error_;
+		});
+
+		proc.on('close', 	code => {
+			if (code === 0) {
+				resolve();
+			} else {
+				reject(error);
+			}
+		});
+	});
 }
 
-function file(options, parse, serialize, path) {
-	let _content = null;
+async function makeFile(options, parse, serialize) {
+	return path => {
+		let content = null;
 
-	return {
-		get content() {
-			if (_content === null) {
-				_content = parse(readFileSync(path, options));
-			}
+		return {
+			async invalidate() {
+				content = null;
+			},
+			async modify(action) {
+				if (content === null) {
+					content = await parse(await readFile(path, options));
+				}
 
-			return _content;
-		},
-		invalidate() {
-			_content = null;
-		},
-		save() {
-			writeFileSync(path, serialize(_content), options);
+				await action(content);
+			},
+			async save() {
+				await	writeFile(path, await serialize(content), options);
 
-			this.invalidate();
-		},
+				await this.invalidate();
+			},
+		};
 	};
 }
