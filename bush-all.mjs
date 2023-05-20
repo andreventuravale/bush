1a21c7859edc5b9c11fbdfe50b99a02dd097d4db
andreventuravale <andreventuravale@gmail.com>
2023-05-20 00:25:47 -0300

    bush-all.mjs |

--- bush-all.mjs
+++ bush-all.mjs
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
 
 import { execSync } from 'node:child_process'
-import { readFile } from 'node:fs/promises'
 import { dirname } from 'node:path'
-
-const makeJsonFile = await makeFile(
-  'utf8',
-  async raw => JSON.parse(raw),
-  async parsed => JSON.stringify(parsed, null, 2)
-)
 
 export async function bushAll () {
   const text = await shell()`find . | grep bush.yaml | grep -v node_modules`
@@ -16,55 +9,11 @@ export async function bushAll () {
   const paths = text.trim().split(/[\r\n]/g)
 
   for await (const path of paths) {
-    const pkg = makeJsonFile(path)
-
-    const name = await pkg.get(({ name }) => name)
-
-    console.log('---> ', name, `( ${path} )`)
+    console.log('==================================')
+    console.log(path)
+    console.log('==================================')
 
     await shell({ cwd: dirname(path), stdio: 'inherit', shell: true })`bush`
-  }
-}
-
-async function makeFile (options, parse, serialize) {
-  return path => {
-    let content = null
-
-    const getContent = async () => {
-      if (content === null) {
-        content = await parse(
-          await readFile(path, options)
-        )
-      }
-
-      return content
-    }
-
-    return {
-      get dir () { return dirname(path) },
-
-      get path () { return path },
-
-      async get (action) {
-        if (!action) {
-          return await getContent()
-        }
-
-        return await action(
-          await getContent()
-        )
-      },
-
-      async invalidate () {
-        content = null
-      },
-
-      async modify (action) {
-        await action(
-          await getContent()
-        )
-      }
-    }
   }
 }
 
